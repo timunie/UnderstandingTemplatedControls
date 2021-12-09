63fb6a44eeba8ac749638fe8708144b1057da699
Tim <tim.uniewski@gmail.com>
2021-12-09 20:01:30 +0100

    Added TemplatedControl
    - Just to show how it works

---
+++ UnderstandingTemplatedControls/Controls/MyControl.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingTemplatedControls.Controls
+{
+    public class MyControl : TemplatedControl
+    {
+
+
+        /// <summary>
+        /// Defines the <see cref="FileName"/> property.
+        /// </summary>
+        public static readonly StyledProperty<string?> FileNameProperty =
+            AvaloniaProperty.Register<MyControl, string?>(nameof(FileName), null, defaultBindingMode: BindingMode.TwoWay);
+
+        /// <summary>
+        ///    This is a sample StyledProperty
+        /// </summary>
+        public string? FileName
+        {
+            get { return GetValue(FileNameProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+    }
+}
