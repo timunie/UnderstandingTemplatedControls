f2ffd208938efba7301ef3be78a0d5b24993fd65
Tim <tim.uniewski@gmail.com>
2021-12-10 19:34:37 +0100

    Added Validation to TemplatedControl
    - Seems like we need a DirectProperty instead of StyledProperty
    - Need to surround ControlTemplate with DataValidationErrors - Control

--- UnderstandingTemplatedControls/Controls/MyControl.cs
+++ UnderstandingTemplatedControls/Controls/MyControl.cs
@@ -1,32 +1,42 @@
 ﻿using Avalonia;
 using Avalonia.Controls.Primitives;
 using Avalonia.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Avalonia.Controls;
 
 namespace UnderstandingTemplatedControls.Controls
 {
     public class MyControl : TemplatedControl
     {
-
+        string? _FileName;
 
         /// <summary>
         /// Defines the <see cref="FileName"/> property.
         /// </summary>
-        public static readonly StyledProperty<string?> FileNameProperty =
-            AvaloniaProperty.Register<MyControl, string?>(nameof(FileName), null, defaultBindingMode: BindingMode.TwoWay);
+        public static readonly DirectProperty<MyControl, string?> FileNameProperty =
+            AvaloniaProperty.RegisterDirect<MyControl, string?>(
+                nameof(FileName),
+                o => o.FileName, 
+                (o, v) => o.FileName = v,
+                defaultBindingMode: BindingMode.TwoWay,
+                enableDataValidation: true);
 
         /// <summary>
         ///    This is a sample StyledProperty
         /// </summary>
         public string? FileName
         {
-            get { return GetValue(FileNameProperty); }
-            set { SetValue(FileNameProperty, value); }
+            get { return _FileName; }
+            set { SetAndRaise(FileNameProperty, ref _FileName, value); }
         }
+
+        protected override void UpdateDataValidation<T>(AvaloniaProperty<T> property, BindingValue<T> value)
+        {
+            if (property == FileNameProperty)
+            {
+                DataValidationErrors.SetError(this, value.Error);
+            }
+        }
+
 
     }
 }
